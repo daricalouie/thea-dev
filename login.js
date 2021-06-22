6d5b18fee28562f44b4170e35f0e37409b8ae238
daricalouie <darica.louie@gmail.com>
2021-06-22 00:00:22 -0700

    Create login.js

---
+++ login.js
@@ -0,0 +1,13 @@
+const loginForm = document.getElementById("login");
+const loginButton = document.getElementById("submit");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const password = loginForm.password.value.toLowerCase();
+
+    if ( password === test) {
+      alert("Password accepted. Press x to close this and leave through door");
+    } else {
+      alert("Incorrect password. This door is only for authorized Nomtanso personnel");
+    }
+})
